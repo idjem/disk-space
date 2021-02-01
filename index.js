cbc37a3afbe01abba2acf2c9778a3b607e1a2e45
Jake Champion <me@jakechampion.name>
2021-02-01 16:43:03 +0000

    add support for osx

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var cp       = require('child_process');
 
 module.exports  = function(mountedon , chain){
-    var ps   = cp.spawn("df", ["-BK" , mountedon]);
+
+    var dfArgs = process.platform === 'darwin' ? '-bk' : "-BK";
+    var ps   = cp.spawn("df", [dfArgs , mountedon]);
     var _ret = "";
 
     ps.stdout.on("data", function(data){
         _ret = data.toString();
     });
-    
+
     ps.on('error' , function(err){
       chain(err)
     });
@@ -18,7 +20,7 @@ module.exports  = function(mountedon , chain){
           var arr = _ret.split('\n')[1].split(/[\s,]+/);
           storageDeviceInfo = {};
           storageDeviceInfo.usedSize  = parseInt(arr[2].replace("K" , "")) * 1024;    // exp "300K" => 300
-          storageDeviceInfo.totalSize = parseInt(arr[3].replace("K" , ""))  * 1024 + storageDeviceInfo.usedSize;        
+          storageDeviceInfo.totalSize = parseInt(arr[3].replace("K" , ""))  * 1024 + storageDeviceInfo.usedSize;
         }
         chain(null , storageDeviceInfo);
     });
